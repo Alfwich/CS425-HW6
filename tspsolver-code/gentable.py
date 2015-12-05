12a1590869779902c5b120bef52090e0329fd058
Arthur Wuterich <arthurwut@gmail.com>
2015-12-05 13:56:07 -0800

    Adds table generation script

---
+++ tspsolver-code/gentable.py
@@ -0,0 +1,23 @@
+import sys, os, random
+
+def main():
+    if len(sys.argv) < 2:
+        print "Useage: 'gentable.py {size}'"
+        return
+
+    size = int(sys.argv[1])
+    print size
+    for row in genTable(size):
+        print row
+
+def genRow(size, pos):
+    row = [ str(random.randint(0,99)) for i in range(size)]
+    row[pos] = "-"
+    return row
+
+def genTable(size):
+    return [" ".join(genRow(size, i)) for i in range(size)]
+
+
+if __name__ == "__main__":
+    main()
